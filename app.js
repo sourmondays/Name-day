219cd04e155dda639d03a4fc3eaba386eab71c9a
sourmondays <58506551+sourmondays@users.noreply.github.com>
2020-01-14 13:21:24 +0100

    Fist commit name day app
    Name day app commit number 0.1

---
+++ app.js
@@ -0,0 +1,42 @@
+const contries = [
+  "cz",
+  "sk",
+  "pl",
+  "fr",
+  "hu",
+  "hr",
+  "se",
+  "us",
+  "at",
+  "it",
+  "es",
+  "de",
+  "dk",
+  "fi"
+];
+
+const timezones = [
+  "America/Denver",
+  "America/Costa_Rica",
+  "America/Los_Angeles",
+  "America/St_Vincent",
+  "America/Toronto",
+  "Europe/Amsterdam",
+  "Europe/Monaco",
+  "Europe/Prague",
+  "Europe/Isle_of_Man",
+  "Africa/Cairo",
+  "Africa/Johannesburg",
+  "Africa/Nairobi",
+  "Asia/Yakutsk",
+  "Asia/Hong_Kong",
+  "Asia/Taipei",
+  "Pacific/Midway",
+  "Pacific/Honolulu",
+  "Etc/GMT-6",
+  "US/Samoa",
+  "Zulu",
+  "US/Hawaii",
+  "Israel",
+  "Etc/GMT-2"
+];
